a7ed9e3240f0c1c46261983666eced280d3775be
milesswu <mileswu13@gmail.com>
2019-09-03 18:29:41 -0700

    Fix navbar behavior
    Navbar collapses when one of its items is clicked. Navbar scroll
    behavior also adjusted to accomodate size of navbar in both mobile and
    desktop environements.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,5 @@
+var navBarHeight;
+
 function setupEventListeners() {
 	window.addEventListener("scroll", function() {
 		if (this.window.scrollY > 10) {
@@ -8,6 +10,7 @@ function setupEventListeners() {
 	});
 
 	$("a.page-scroll").click(function() {
+		//checks that path exists and that element is from the same domain as the site
 		if (
 			location.pathname.replace(/^\//, "") ==
 				this.pathname.replace(/^\//, "") &&
@@ -20,22 +23,32 @@ function setupEventListeners() {
 			target = target.length
 				? target
 				: $("[name=" + this.hash.slice(1) + "]");
-			console.log(this, target);
+			// console.log(this, target);
+			console.log("1", document.querySelector("nav").offsetHeight);
 			if (target.length) {
+				console.log("2", document.querySelector("nav").offsetHeight);
 				$("html, body").animate(
 					{
-						scrollTop:
-							target.offset().top -
-							document.querySelector("nav").offsetHeight,
+						scrollTop: target.offset().top - navBarHeight,
 					},
 					900
 				);
+				console.log("3", document.querySelector("nav").offsetHeight);
 				return false;
 			}
 		}
 	});
+
+	$(".navbar-nav>li>a").on("click", function() {
+		console.log("collapsed");
+		$(".navbar-collapse").collapse("hide");
+	});
 }
 
 window.onload = function() {
+	if (this.window.scrollY > 10) {
+		this.document.querySelector("nav").classList.add("nav-scroll");
+	}
+	navBarHeight = document.querySelector("nav").offsetHeight;
 	setupEventListeners();
 };
